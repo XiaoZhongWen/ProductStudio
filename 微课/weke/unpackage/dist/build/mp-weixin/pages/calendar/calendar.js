19c2a79086c35e74e19281c8f3fcbc0dedf3ffc8
xiaozhongwen <zhongwenxiao@hotmail.com>
2023-08-17 22:09:46 +0800

    1. ts配置 2. pinia 3. uni-ui

--- 微课/weke/unpackage/dist/build/mp-weixin/pages/calendar/calendar.js
+++ 微课/weke/unpackage/dist/build/mp-weixin/pages/calendar/calendar.js
@@ -1,1 +1,1 @@
-"use strict";require("../../common/vendor.js");const e={__name:"calendar",setup:e=>(e,r)=>({})};wx.createPage(e);
+"use strict";const e={};const r=require("../../common/vendor.js")._export_sfc(e,[["render",function(e,r){return{}}]]);wx.createPage(r);
