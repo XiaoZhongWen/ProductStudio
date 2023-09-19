0b743caf23da4addc266845420861a0b60cd1711
XiaoZhongWen <zhongwen2008.gd@163.com>
2023-09-19 21:37:18 +0800

    Update index.obj.js

--- 微课/weke/uniCloud-tcb/cloudfunctions/users/index.obj.js
+++ 微课/weke/uniCloud-tcb/cloudfunctions/users/index.obj.js
@@ -1,8 +1,7 @@
 // 云对象教程: https://uniapp.dcloud.net.cn/uniCloud/cloud-obj
 // jsdoc语法提示教程：https://ask.dcloud.net.cn/docs/#//ask.dcloud.net.cn/article/129
 const mp_wx_data = {
-	AppID:'wx53884ee4fcbb9a5b',
-	AppSecret:'5bf343c79753aa2f2d4e9283886b2aa3'
+	AppID:'wx53884ee4fcbb9a5b'
 }
 module.exports = {
 	_before: function () { // 通用预处理器
